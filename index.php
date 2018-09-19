7b3d8f71498881128125e48b340cf750765a96de
Elijah Baird <ebaird@csumb.edu>
2018-09-19 22:20:27 +0000

    Added empty functions.php file as well as index.php with basic starter code

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include "functions.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1> Silverjack </h1>
+    </head>
+    <body>
+        Body stuff
+    </body>
+    <hr>
+    <footer>
+        ST 336 2018 &copy; Baird, Chi, and Anacleto <br/>
+        Disclaimer: This website is for academic purposes only. <br/>
+        <img src="../cst336/img/csumb_logo.jpg" alt="CSUMB logo" title="This is the CSUMB logo"/>
+    </footer>
+</html>
