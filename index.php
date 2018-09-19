3fa143d4e5d8474aa7a237460b34ec8948797ee0
Elijah Baird <ebaird@csumb.edu>
2018-09-19 22:28:37 +0000

    Added css folder and empty css file, as well as fixed a typo in index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     </body>
     <hr>
     <footer>
-        ST 336 2018 &copy; Baird, Chi, and Anacleto <br/>
+        CST 336 2018 &copy; Baird, Chi, and Anacleto <br/>
         Disclaimer: This website is for academic purposes only. <br/>
         <img src="../cst336/img/csumb_logo.jpg" alt="CSUMB logo" title="This is the CSUMB logo"/>
     </footer>
